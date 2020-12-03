0a4fbced665feb8516170dc185c25fe76740b538
luisespinosav97 <69775125+luisespinosav97@users.noreply.github.com>
2020-12-03 15:40:30 -0500

    Add files via upload

---
+++ Final/stream/stream.sh
@@ -0,0 +1,17 @@
+sudo -i 
+yum -y update
+yum -y install vim wget
+wget --no-cookies --no-check-certificate --header "Cookie:oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
+yum -y localinstall jdk-8u131-linux-x64.rpm
+java -version
+wget https://github.com/dularion/streama/releases/download/v1.1/streama-1.1.war
+mkdir /opt/streama
+mv streama-1.1.war /opt/streama/streama.war
+java -jar /opt/streama/streama.war
+mkdir /opt/streama/media
+chmod 664 /opt/streama/media
+sudo cp /vagrant/stream/streama.service /etc/systemd/system/streama.service
+sudo cp /vagrant/stream/I-Like-It /opt/streama/media/
+systemctl start streama
+systemctl enable streama
+systemctl status streama
